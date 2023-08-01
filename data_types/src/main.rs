d60aa580054d140c37b36f39dae582a0fbf53de8
teddywafula <teddywafula@gmail.com>
2023-08-01 14:21:10 +0300

    RUST-101-DATATYPES

---
+++ data_types/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // char value
+    let x:char = 'a';
+    println!("Char value is {}", x);
+    // bool value
+    let y:bool = false;
+    println!("Boolean value is {}", y);
+    // float value with single precision
+    let z:f32 = 23.11;
+    println!("Float value is {}", z);
+    // float value with double precision
+    let p:f64 = 24.1133232323;
+    println!("Float value is {}", p);
+
+}
